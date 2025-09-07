0f074bd2239b017f59e40ce1ecf5a4e1fb382aa9
magnu <magnu@COMPUTOR>
2025-09-07 08:54:33 +0200

    Add project files.

---
+++ passwordencrypter/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace passwordencrypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.passwords = new System.Windows.Forms.ListBox();
+            this.directorylabel = new System.Windows.Forms.Label();
+            this.encryptbtn = new System.Windows.Forms.Button();
+            this.writebutton = new System.Windows.Forms.Button();
+            this.decryptbtn = new System.Windows.Forms.Button();
+            this.filecontent = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.filePassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.createPassFile = new System.Windows.Forms.Button();
+            this.fileName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.changedirbutton = new System.Windows.Forms.Button();
+            this.filteringButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // passwords
+            // 
+            this.passwords.FormattingEnabled = true;
+            this.passwords.Location = new System.Drawing.Point(12, 12);
+            this.passwords.Name = "passwords";
+            this.passwords.Size = new System.Drawing.Size(156, 420);
+            this.passwords.TabIndex = 0;
+            this.passwords.SelectedIndexChanged += new System.EventHandler(this.passwords_SelectedIndexChanged);
+            // 
+            // directorylabel
+            // 
+            this.directorylabel.AutoSize = true;
+            this.directorylabel.Location = new System.Drawing.Point(174, 12);
+            this.directorylabel.Name = "directorylabel";
+            this.directorylabel.Size = new System.Drawing.Size(243, 13);
+            this.directorylabel.TabIndex = 1;
+            this.directorylabel.Text = "C:\\Users\\magnu\\AppData\\Local\\MaggiEncryptor";
+            // 
+            // encryptbtn
+            // 
+            this.encryptbtn.Location = new System.Drawing.Point(339, 409);
+            this.encryptbtn.Name = "encryptbtn";
+            this.encryptbtn.Size = new System.Drawing.Size(75, 23);
+            this.encryptbtn.TabIndex = 2;
+            this.encryptbtn.Text = "Encrypt";
+            this.encryptbtn.UseVisualStyleBackColor = true;
+            this.encryptbtn.Click += new System.EventHandler(this.encryptbtn_Click);
+            // 
+            // writebutton
+            // 
+            this.writebutton.Location = new System.Drawing.Point(203, 409);
+            this.writebutton.Name = "writebutton";
+            this.writebutton.Size = new System.Drawing.Size(49, 23);
+            this.writebutton.TabIndex = 3;
+            this.writebutton.Text = "Write";
+            this.writebutton.UseVisualStyleBackColor = true;
+            this.writebutton.Click += new System.EventHandler(this.writebutton_Click);
+            // 
+            // decryptbtn
+            // 
+            this.decryptbtn.Location = new System.Drawing.Point(258, 409);
+            this.decryptbtn.Name = "decryptbtn";
+            this.decryptbtn.Size = new System.Drawing.Size(75, 23);
+            this.decryptbtn.TabIndex = 4;
+            this.decryptbtn.Text = "Decrypt";
+            this.decryptbtn.UseVisualStyleBackColor = true;
+            this.decryptbtn.Click += new System.EventHandler(this.decryptbtn_Click);
+            // 
+            // filecontent
+            // 
+            this.filecontent.Location = new System.Drawing.Point(177, 139);
+            this.filecontent.Multiline = true;
+            this.filecontent.Name = "filecontent";
+            this.filecontent.Size = new System.Drawing.Size(349, 195);
+            this.filecontent.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(174, 337);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // filePassword
+            // 
+            this.filePassword.Location = new System.Drawing.Point(177, 353);
+            this.filePassword.Name = "filePassword";
+            this.filePassword.Size = new System.Drawing.Size(349, 20);
+            this.filePassword.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Content";
+            // 
+            // createPassFile
+            // 
+            this.createPassFile.Location = new System.Drawing.Point(177, 410);
+            this.createPassFile.Name = "createPassFile";
+            this.createPassFile.Size = new System.Drawing.Size(20, 20);
+            this.createPassFile.TabIndex = 9;
+            this.createPassFile.Text = "+";
+            this.createPassFile.UseVisualStyleBackColor = true;
+            this.createPassFile.Click += new System.EventHandler(this.createPassFile_Click);
+            // 
+            // fileName
+            // 
+            this.fileName.Location = new System.Drawing.Point(177, 100);
+            this.fileName.Name = "fileName";
+            this.fileName.Size = new System.Drawing.Size(349, 20);
+            this.fileName.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(174, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(451, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Safe mode";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // changedirbutton
+            // 
+            this.changedirbutton.Location = new System.Drawing.Point(174, 28);
+            this.changedirbutton.Name = "changedirbutton";
+            this.changedirbutton.Size = new System.Drawing.Size(98, 23);
+            this.changedirbutton.TabIndex = 13;
+            this.changedirbutton.Text = "Change";
+            this.changedirbutton.UseVisualStyleBackColor = true;
+            this.changedirbutton.Click += new System.EventHandler(this.changedirbutton_click);
+            // 
+            // filteringButton
+            // 
+            this.filteringButton.Location = new System.Drawing.Point(174, 57);
+            this.filteringButton.Name = "filteringButton";
+            this.filteringButton.Size = new System.Drawing.Size(98, 23);
+            this.filteringButton.TabIndex = 14;
+            this.filteringButton.Text = "Filtering True";
+            this.filteringButton.UseVisualStyleBackColor = true;
+            this.filteringButton.Click += new System.EventHandler(this.filteringButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 446);
+            this.Controls.Add(this.filteringButton);
+            this.Controls.Add(this.changedirbutton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.fileName);
+            this.Controls.Add(this.createPassFile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.filePassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.filecontent);
+            this.Controls.Add(this.decryptbtn);
+            this.Controls.Add(this.writebutton);
+            this.Controls.Add(this.encryptbtn);
+            this.Controls.Add(this.directorylabel);
+            this.Controls.Add(this.passwords);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Maggi\'s password encryptor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox passwords;
+        private System.Windows.Forms.Label directorylabel;
+        private System.Windows.Forms.Button encryptbtn;
+        private System.Windows.Forms.Button writebutton;
+        private System.Windows.Forms.Button decryptbtn;
+        private System.Windows.Forms.TextBox filecontent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox filePassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button createPassFile;
+        private System.Windows.Forms.TextBox fileName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button changedirbutton;
+        private System.Windows.Forms.Button filteringButton;
+    }
+}
+
---
+++ passwordencrypter/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace passwordencrypter
+{
+    public partial class Form1 : Form
+    {
+        public string passwordsPath = @"C:\Users\magnu\AppData\Local\MaggiEncryptor";
+        public string selectedFile;
+        public System.Windows.Forms.ToolTip cryptographytip = new System.Windows.Forms.ToolTip();
+        public bool filteringEnabled = true;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.MaximizeBox = false;
+            Directory.CreateDirectory(passwordsPath);
+            string[] files = Directory.GetFiles(passwordsPath);
+            foreach (string file in files)
+            {
+                passwords.Items.Add(Path.GetFileName(file));
+            }
+            if (passwords.Items.Count >= 1)
+            {
+                passwords.SelectedIndex = 0;
+                selectedFile = passwords.Items[passwords.SelectedIndex].ToString();
+            }
+        }
+        private void refreshList()
+        {
+            string[] files = Directory.GetFiles(passwordsPath);
+            passwords.Items.Clear();
+            foreach (string file in files)
+            {
+                if (File.)
+                {
+
+                }
+                else
+                {
+                    passwords.Items.Add(Path.GetFileName(file));
+                }
+
+            }
+            if (passwords.Items.Count >= 1)
+            {
+                passwords.SelectedIndex = 0;
+                selectedFile = passwords.Items[passwords.SelectedIndex].ToString();
+            }
+        }
+        private void showFile(string filepath)
+        {
+            if (filepath == null)
+            {
+                fileName.Text = null;
+                filecontent.Text = null;
+                fileName.Enabled = false;
+                filecontent.Enabled = false;
+                filePassword.Enabled = false;
+            }
+            else 
+            {
+                if (File.Exists(filepath))
+                {
+                    selectedFile = filepath;
+                    string content = File.ReadAllText(filepath);
+                    filecontent.Text = content;
+                    fileName.Text = Path.GetFileNameWithoutExtension(filepath);
+                    fileName.Enabled = true;
+                    filecontent.Enabled = true;
+                    filePassword.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("No file found.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private byte[] cryptographyfunc(string pass, string data, bool decrypt)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                var key = new Rfc2898DeriveBytes(pass, Encoding.UTF8.GetBytes("randomsaltstuff255252336246414234"));
+                aes.Key = key.GetBytes(32);
+                aes.IV = key.GetBytes(16);
+
+                if (!decrypt) // ENCRYPT
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter sw = new StreamWriter(cs))
+                            {
+                                sw.Write(data);
+                            }
+                        }
+                        return ms.ToArray();
+                    }
+                }
+                else // DECRYPT
+                {
+                    byte[] cipherBytes = Convert.FromBase64String(data);
+                    using (MemoryStream ms = new MemoryStream(cipherBytes))
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                        {
+                            using (StreamReader sr = new StreamReader(cs))
+                            {
+                                string plain = sr.ReadToEnd();
+                                return Encoding.UTF8.GetBytes(plain); // return as byte[] like encrypt
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void encryptbtn_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(filePassword.Text) && !string.IsNullOrEmpty(filecontent.Text))
+            {
+                string password = filePassword.Text;
+                string content = filecontent.Text;
+                byte[] encryptedData = cryptographyfunc(filePassword.Text, content, false);
+                filecontent.Text = Convert.ToBase64String(encryptedData);
+                System.Windows.Forms.ToolTip tooltip1 = new System.Windows.Forms.ToolTip();
+                tooltip1.IsBalloon = true;
+                tooltip1.Show("Press this to save", writebutton, 0, -40, 3000);
+
+            }
+            else
+            {
+                MessageBox.Show("Password missing.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+
+        }
+
+        private void decryptbtn_Click(object sender, EventArgs e)
+        {
+            string password; // make sure the file name is valid
+            if (!string.IsNullOrEmpty(filePassword.Text))
+            {
+                password = filePassword.Text;
+                byte[] decryptedData = cryptographyfunc(filePassword.Text, filecontent.Text, true);
+                filecontent.Text = Encoding.UTF8.GetString(decryptedData); // <-- FIXED
+
+
+                cryptographytip.IsBalloon = true;
+                cryptographytip.Show("Press this to save", writebutton, 0, -40, 3000);
+
+            }
+            else
+            {
+                MessageBox.Show("Password missing.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void writebutton_Click(object sender, EventArgs e)
+        {
+            string newName = fileName.Text + ".crypt";
+            string newContent = filecontent.Text;
+            string newPath = Path.Combine(passwordsPath, newName);
+            if (selectedFile != null && File.Exists(newPath))
+            {
+
+                passwords.SelectedIndexChanged -= passwords_SelectedIndexChanged; // unsubscribe so it doesnt cause issues
+                passwords.Items[passwords.SelectedIndex] = newName;
+                passwords.SelectedIndexChanged += passwords_SelectedIndexChanged; // resubscribe
+                File.WriteAllText(selectedFile, newContent);
+                File.Move(selectedFile, newPath);
+                showFile(newPath); // show the file
+            }
+        }
+
+        private void passwords_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(passwords.SelectedIndex >= 0)
+            {
+                string filepath = Path.Combine(passwordsPath, passwords.SelectedItem.ToString());
+                showFile(filepath);
+            }
+        }
+
+        private void createPassFile_Click(object sender, EventArgs e)
+        {
+
+
+
+            if (passwords.SelectedIndex >= 0) //check if something is already selected
+            {
+                passwords.SelectedIndex = -1;
+                showFile(null); // clear the labels
+                MessageBox.Show("You can now name the new file.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                fileName.Enabled = true;
+            }
+            else //if not, attempt to make a new file
+            {
+                string filename; // make sure the file name is valid
+                if (fileName.Text == null || fileName.Text == "")
+                {
+                    filename = "default";
+                }
+                else
+                {
+                    filename = fileName.Text.Trim().Replace(".", "");
+                }
+
+                string newfile = Path.Combine(passwordsPath, filename + ".crypt"); // create path
+
+                if (File.Exists(newfile)) // check if it already exists
+                {
+                    MessageBox.Show("File already exists.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    File.WriteAllText(newfile, "");
+                    passwords.Items.Add(Path.GetFileName(newfile));
+                    showFile(newfile); // show the file
+                }
+            }
+        }
+
+        private void changedirbutton_click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog newdirdialog = new FolderBrowserDialog())
+            {
+                DialogResult = newdirdialog.ShowDialog(this);
+
+                if (DialogResult == DialogResult.OK)
+                {
+                    string folderPath = newdirdialog.SelectedPath;
+                    if (Directory.Exists(folderPath)) 
+                    {
+                        passwordsPath = folderPath;
+                        directorylabel.Text = folderPath;
+                        refreshList();
+                    }
+                }
+            }
+        }
+
+        private void filteringButton_Click(object sender, EventArgs e)
+        {
+            filteringEnabled = !filteringEnabled;
+            filteringButton.Text = "Filtering "+filteringEnabled;
+        }
+    }
+}
